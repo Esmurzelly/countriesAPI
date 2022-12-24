f9ffd4fcffc8975e125d4f4a100159326034131a
Esmurzelly <78688219+Esmurzelly@users.noreply.github.com>
2022-12-24 19:17:51 +0300

    filterSearch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { IoMoon, IoMoonOutline } from "react-icons/io5";
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import { Container } from './Container';
 
@@ -15,8 +16,8 @@ const Wrapper = styled.div`
     align-items: center;
     padding: 2rem 0;
 `;
-const Title = styled.a.attrs({
-    href: '/',
+const Title = styled(Link).attrs({
+    to: '/',
 })`
     color: var(--colors-text);
     font-size: var(--fs-sm);
---
+++ src/pages/Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Details = () => {
+    let {name} = useParams();
+
+    return <div>Details {name}</div>
+
+}
+ 
+export default Details;
